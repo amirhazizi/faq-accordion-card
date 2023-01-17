1afd74922dc99a023646b243b9d729e6cc2d5ffb
amir <amirazizihosein@gmail.com>
2023-01-17 21:13:52 +0330

    update index.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,10 +15,6 @@ module.exports = {
       },
       dropShadow: {
         "3xl": "0 25px 0 rgba(0, 0, 0, .07)",
-        "4xl": [
-          "0 35px 35px rgba(0, 0, 0, 0.25)",
-          "0 45px 65px rgba(0, 0, 0, 0.15)",
-        ],
       },
       height: {
         152: "37rem",
